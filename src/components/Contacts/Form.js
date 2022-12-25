b8bcb405875210b0117c94490ee98f1d4c9e313e
Ebrar <16155052@mersin.edu.tr>
2022-12-25 19:34:21 +0300

    add styled

--- src/components/Contacts/Form.js
+++ src/components/Contacts/Form.js
@@ -5,28 +5,29 @@ import {nanoid} from "@reduxjs/toolkit";
 
 function Form() {
   const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
       e.preventDefault(); //native davranışı engelliyoruz, submit olunca sayfanın yenilenmesini engellemek icin.
     //   console.log("saASa") ; //submit olduğunu console'dan kontrol ettim.
 
-    if(!name) return false; //name yoksa çalışma.
+    if(!name ||  !number) return false; //name ve number  yoksa çalışma.
 
-    // const names = name.split(",");
-    // names.forEach((name) =>  dispatch(addContact({id: nanoid(), name})));
-
-    // const data = names.map((name) => ({id: nanoid(), name}));
-    // dispatch(addContacts(data)); 
-    
-    dispatch(addContact({id: nanoid(), name}));
+   
+    dispatch(addContact({id: nanoid(), name, phone_number: number}));
     setName("");//input boşaltmak için
+    setNumber("");//input boşaltmak için
   }
 
   return (
     <div>
         <form onSubmit={handleSubmit}>
             <input placeholder='name' value={name} onChange={(e) => setName(e.target.value)}/>
+            <input type="number" placeholder='phone number' value={number} onChange={(e) => setNumber(e.target.value)}/>
+           <div className='btn'>
+           <button type='submit'>Add</button>
+            </div>
         </form>
     </div>
   )
